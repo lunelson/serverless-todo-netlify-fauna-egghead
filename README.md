28808a1ef318d54c278a220baf8f414c2023b286
Lu Nelson <lunelson@gmail.com>
2020-02-18 17:29:14 +0100

    add identity

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Serverless TODOs with Netlify and FaunaDB
+
+## Theme UI
+
+## Netlify Identity
+
+- is enabled on the project, 4th tab
+
+Auth Widget
+Auth Widget and Demo
+https://github.com/netlify/netlify-identity-widget
+https://identity.netlify.com/
--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Heading, Button, Flex } from 'theme-ui';
+import netlifyIdentity from 'netlify-identity-widget'
 
-export default (props) => (
-  <Container>
-    <Flex sx={{ flexDirection: 'column', padding: 3 }}>
-      <Heading as="h1">Get Stuff Done</Heading>
-      <Button
-        sx={{ marginTop: 2 }}
-        onClick={e => { alert('clicked') }}
-      >Log In</Button>
-    </Flex>
-  </Container>
-);
+
+export default (props) => {
+  useEffect(() => {
+    netlifyIdentity.init({});
+  })
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: 'column', padding: 3 }}>
+        <Heading as="h1">Get Stuff Done</Heading>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={e => {
+            netlifyIdentity.open();
+          }}
+        >Log In</Button>
+      </Flex>
+    </Container>
+  )
+}
